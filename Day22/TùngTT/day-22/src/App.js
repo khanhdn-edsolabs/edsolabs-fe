10cec8918de936db79179c112a32202c9bbb0d2b
TungTT <truongthanhtung2701.dev@gmail.com>
2021-09-08 20:47:49 +0700

    Update App.js
    sửa img tag => self close tag

--- Day22/TùngTT/day-22/src/App.js
+++ Day22/TùngTT/day-22/src/App.js
@@ -15,13 +15,13 @@ function App() {
               9:41
             </div>
             <div className="navbar__battery">
-              <img src={cellular} className="navbar__icon navbar--icon--cellular" alt="Cellular"></img>
-              <img src={wifi} className="navbar__icon navbar--icon--wifi" alt="Wifi"></img>
-              <img src={battery} className="navbar__icon navbar--icon--battery" alt="Battery"></img>
+              <img src={cellular} className="navbar__icon navbar--icon--cellular" alt="Cellular"/>
+              <img src={wifi} className="navbar__icon navbar--icon--wifi" alt="Wifi"/>
+              <img src={battery} className="navbar__icon navbar--icon--battery" alt="Battery"/>
             </div>
           </div>
           <div className="container__logo">
-            <img className="logo" src={logo} alt="Logo"></img>
+            <img className="logo" src={logo} alt="Logo"/>
             <div className="container__logo__text">
               <p>Welcome back!</p>
               <h1>Please, Log In.</h1>
@@ -31,12 +31,12 @@ function App() {
         <div className="container__content">
           <form className="input__field">
             <div className="btn input--content">
-              <img src={ user } alt="user"></img>
-              <input type="text" placeholder="johnsondoe@nomail.com"></input>
+              <img src={ user } alt="user"/>
+              <input type="text" placeholder="johnsondoe@nomail.com"/>
             </div>
             <div className="btn input--content">
-              <img src={key} alt="key"></img>
-              <input type="password" minlength="8" required placeholder="*****************"></input>
+              <img src={key} alt="key"/>
+              <input type="password" minlength="8" required placeholder="*****************"/>
             </div>
             <div className="btn input--content">
               <button className="input__btn sign--in--btn" type="submit">Continue ></button>
