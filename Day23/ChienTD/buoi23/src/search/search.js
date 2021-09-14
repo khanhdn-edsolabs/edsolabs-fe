4b2f02cbe01fcbd03c784e6200c201a4a08afef9
Chien Ta Duc <81642308+chienomh@users.noreply.github.com>
2021-09-14 09:02:36 +0700

    ChienTD (#134)

---
+++ Day23/ChienTD/buoi23/src/search/search.js
@@ -0,0 +1,103 @@
+import "./search.css";
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import SearchIcon from "@material-ui/icons/Search";
+import Box from "@material-ui/core/Box";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "65ch",
+    },
+  },
+}));
+
+// Style boder
+const defaultProps = {
+  bgcolor: "background.paper",
+  borderColor: "text.primary",
+  m: 1,
+  border: 1,
+  style: { width: "40%", height: "3rem" },
+};
+
+const Search = (props) => {
+  const [enterCity, setEnterCity] = useState("");
+  const [loading, setLoading] = useState(false);
+  const enteredHandler = (e) => {
+    setEnterCity(e.target.value);
+  };
+
+  const classes = useStyles();
+
+  // Call API
+  // const api = environment.startAPI+enterCity+environment.endAPI;
+  const api =
+    process.env.REACT_APP_URL_START + enterCity + process.env.REACT_APP_URL_END;
+
+  const callApi = (event) => {
+    setLoading(true);
+    event.preventDefault();
+    fetch(api)
+      .then((res) => res.json())
+      .then((data) => {
+        props.getRegionalWeather(data);
+        setLoading(false);
+      });
+  };
+
+  // useEffect(() => {
+  //   setLoading(true);
+  // }, []);
+
+  return (
+    <Box
+      className="box-search"
+      textAlign="center"
+      width="100%"
+      display="inline-block"
+    >
+      <h1>EdsoLabs 3-Day Forecast</h1>
+      <Box
+        borderRadius={50}
+        {...defaultProps}
+        display="inline-block"
+        className="wrapper-input-search"
+      >
+        <Box display="flex" width="80%">
+          <SearchIcon className="icon-search" />
+          <form
+            onSubmit={callApi}
+            className={classes.root}
+            noValidate
+            autoComplete="off"
+          >
+            <TextField
+              placeholder="Nhập thành phố muốn xem"
+              onChange={enteredHandler}
+              className="input"
+            />
+          </form>
+        </Box>
+      </Box>
+      <br />
+      {loading && (
+        <Box
+          component="h3"
+          display="flex"
+          textAlign="center"
+          justifyContent="center"
+          alignItems='center'
+        >
+          <CircularProgress/>
+          <Box ml={2}>Getting information, please wait ...</Box>
+        </Box>
+      )}
+    </Box>
+  );
+};
+
+export default Search;
---
+++ Day23/ChienTD/buoi23/src/today/today.js
@@ -0,0 +1,65 @@
+import "./today.css";
+import React, { useState, useEffect } from "react";
+import { Box } from "@material-ui/core";
+
+// Style boder
+const defaultProps = {
+  bgcolor: "background.paper",
+  borderColor: "text.primary",
+  m: 1,
+  border: 1,
+  style: { width: "40rem", height: "15rem" },
+};
+
+const Today = (props) => {
+  const [name, setName] = useState();
+  const [country, setCountry] = useState();
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    if (props && props.weather.location) {
+      const oldName = props.weather.location.name;
+      const oldCountry = props.weather.location.country;
+      setName(oldName);
+      setCountry(oldCountry);
+      setData(props);
+    }
+  }, [props]);
+
+  return (
+    <Box
+      {...defaultProps}
+      display="inline-block"
+      borderRadius={20}
+      width="100%"
+    >
+      <h2>
+        Today's Weather in {name}, {country}
+      </h2>
+      {data ? (
+        <Box className="content-weather" display="flex" width={1}>
+          <Box className="temperature" display="flex" width="60%" ml={4}>
+            <Box width="50%">
+              <img
+                src={data.weather.current.condition.icon}
+                alt="this is icon"
+                width="100%"
+              />
+            </Box>
+            <Box width="40%" textAlign="left" pt={3}>
+              <h3>{data.weather.current.condition.text}</h3>
+              <h2>{data.weather.current.temp_c} °C</h2>
+            </Box>
+          </Box>
+          <Box className="additional-weather" width="50%" textAlign="left">
+            <h3>Wind: {data.weather.current.wind_kph} kmph</h3>
+            <h3>Pricip: {data.weather.current.precip_mm} mm</h3>
+            <h3>Pressure: {data.weather.current.pressure_mb} mb</h3>
+          </Box>
+        </Box>
+      ) : null}
+    </Box>
+  );
+};
+
+export default Today;
