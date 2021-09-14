3157d6659faa9975acc34f4ddb760c1f251fe133
phantrung1997 <71858832+phantrung1997@users.noreply.github.com>
2021-09-14 09:14:26 +0700

    weatherapp (#139)

---
+++ Day23/Trungpv/weather-app/src/components/Weather3day.js
@@ -0,0 +1,24 @@
+import moment from "moment";
+import { Fragment } from "react";
+
+export default function weather3day(e) {
+  const { city } = e;
+  return (
+    <Fragment>
+       <div className="weather3day">
+            {city.forecast.forecastday.map((items) => {
+              return (
+                <div key={items.date_epoch} className="box">
+                  <p className="day">{moment(`${items.date}`).format(`ddd`)}</p>
+                  <p className="time">
+                    {moment(`${items.date}`).format(`DD/MM`)}
+                  </p>
+                  <img src={items.day.condition.icon} alt=""></img>
+                  <p>{items.day.avgtemp_c} °c</p>
+                </div>
+              );
+            })}
+          </div>
+    </Fragment>
+  );
+}
