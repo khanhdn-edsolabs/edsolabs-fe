23dcbb5e93d62a06f5ca28f296e0fddf2efc1e7d
pdnm20091998 <pdnm1998@gmail.com>
2021-09-24 10:06:58 +0700

    login

--- Day29/PhungDacNhatMinh/liststudent/src/Api/api.js
+++ Day29/PhungDacNhatMinh/liststudent/src/Api/api.js
@@ -3,3 +3,8 @@ import axios from "axios";
 export const getStudent = async () => {
   return await axios.get(`${process.env.REACT_APP_API_STUDENT}`);
 };
+export const getUser = async (username, password) => {
+  return await axios.get(`${process.env.REACT_APP_API_USERS}`, {
+    params: { username, password },
+  });
+};
--- Day29/PhungDacNhatMinh/liststudent/src/Layout/login/Login.js
+++ Day29/PhungDacNhatMinh/liststudent/src/Layout/login/Login.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { FloatingLabel, Form, Button, Card } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
+import { getUser } from "../../Api/api";
 // import PropTypes from 'prop-types';
 
 Login.propTypes = {};
 
 function Login(props) {
   const history = useHistory();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const aut = async () => {
+    const res = await getUser(username, password);
+    const { data } = res;
+    console.log(data);
+    if (data.length === 0 || data.length > 1) {
+      alert("Tài khoản hoặc mật khẩu không chính xác");
+      return;
+    }
+    if (username === "admin@gmail.com") {
+      history.push("/");
+    } else {
+      alert("Tài khoản không tồn tại");
+    }
+  };
   const handleClick = (e) => {
-    localStorage.setItem("user", "admin@gmail.com");
-    history.push("/");
+    localStorage.setItem("user", username);
+    aut();
   };
   return (
     <main className="container d-flex justify-content-center align-items-center">
@@ -21,15 +38,25 @@ function Login(props) {
             label="Email address"
             className="mb-3"
           >
-            <Form.Control type="email" placeholder="name@example.com" />
+            <Form.Control
+              type="email"
+              placeholder="name@example.com"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
           </FloatingLabel>
           <FloatingLabel controlId="floatingPassword" label="Password">
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </FloatingLabel>
           <Button
             variant="outline-dark"
             className="mt-3"
-            onClick={() => handleClick()}
+            onClick={(e) => handleClick(e)}
           >
             Login
           </Button>
