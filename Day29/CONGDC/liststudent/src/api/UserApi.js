6c6ade78a65384c2e9d5a8cc8a8be1e2477a61bc
bangpham <phamvanbang1202@gmail.com>
2021-09-28 09:49:51 +0700

    Revert "Day29-CONGDC (#204)"

--- Day29/CONGDC/liststudent/src/api/UserApi.js
+++
@@ -1,9 +0,0 @@
-const api = "http://localhost:3001/users";
-const getAllUser = (callback) => {
-  fetch(api)
-    .then((res) => res.json())
-    .then(callback)
-    .catch((error) => console.log("Error:", error));
-};
-
-export { getAllUser };
